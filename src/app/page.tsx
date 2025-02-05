f71219e4d87d18ba296dd9e2ef4f7e8703e4015b
Raimundo Henriques <raimundo.henriques@gmail.com>
2025-02-05 16:52:32 +0100

    feat: merge landing and session pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { Session } from "@/lib/types";
 import { redirect } from "next/navigation";
 import { v4 as uuidv4 } from "uuid";
-import Image from "next/image";
 import { sql } from "@/lib/db";
 import { SubmitButton } from "@/components/SubmitButton";
 
 export default function Home() {
-  async function createSession() {
+  async function startSession(formData: FormData) {
     "use server";
 
     const session: Session = {
@@ -19,34 +18,79 @@ export default function Home() {
       VALUES (${session.sessionID}, ${session.status})
     `;
 
-    redirect(`/session/${session.sessionID}`);
+    const id = session.sessionID;
+    const valueName = formData.get("value_name") as string;
+    const description = formData.get("description") as string;
+    const intervalRange = formData.get("interval_range") as string;
+
+    await sql`
+      UPDATE sessions
+      SET value_name = ${valueName}, description = ${description}, interval_range = ${intervalRange}
+      WHERE session_id = ${id}
+    `;
+
+    redirect(`/session/${id}/links`);
   }
 
+  const inputClasses =
+    "border-4 border-sine-purple rounded-xl px-2 text-right mx-1.5";
+
   return (
-    <div className="flex flex-col justify-between items-center gap-4">
+    <div className="flex flex-col justify-between items-center gap-4 max-w-2xl">
       <h1>Private Benchmark</h1>
-      <p className="max-w-2xl">
+      <p>
         This platform let&apos;s you benchmark the values of three participants{" "}
         <strong>without disclosing their inputs</strong> to each other, nor to a
         third party.
       </p>
-      <p className="max-w-2xl">
+      <p>
         Sounds impossible? Not with <strong>Polytune</strong>, SINE
         Foundation&apos;s Secure Multiparty Computation (MPC) engine.
       </p>
-      <Image
-        src={"/smpc.png"}
-        alt={"SMPC example"}
-        width={1000}
-        height={1000}
-      />
-      <p className="max-w-2xl">
+      <p>
         SINE Foundation brings advanced cryptography to everyone. You can use it
         in the browser, without any technical knowledge.
       </p>
-      <form action={createSession}>
-        <SubmitButton>Give it a try!</SubmitButton>
-      </form>
+      <div className="border border-black rounded-3xl p-4 mt-6">
+        <p className="text-xl font-bold mb-6">Give it a try!</p>
+        <form
+          action={startSession}
+          className="flex flex-col gap-4 items-center"
+        >
+          <p className="text-l max-w-2xl text-center leading-8">
+            For 3 participants, check whether their
+            <input
+              type="text"
+              id="valueName"
+              name="value_name"
+              className={`w-36 ${inputClasses}`}
+              placeholder="CO2e emissions"
+              required
+            />
+            in
+            <input
+              type="text"
+              id="description" // TODO: change to UNIT
+              name="description" // TODO: Change to UNIT
+              required
+              placeholder="kg"
+              className={`w-14 ${inputClasses}`}
+            />
+            are within a
+            <input
+              type="number"
+              id="intervalRange"
+              name="interval_range"
+              className={`w-16 ${inputClasses}`}
+              placeholder="10"
+              required
+            />
+            % range of each other. <em>Privately</em>, using strong encryption
+            built by SINE Foundation.
+          </p>
+          <SubmitButton>Start</SubmitButton>
+        </form>
+      </div>
     </div>
   );
 }

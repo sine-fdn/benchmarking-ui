80efc9f3de35906cdc3fbed3ef7f0d6ed556ab01
Raimundo Henriques <raimundo.henriques@gmail.com>
2025-02-10 09:50:44 +0100

    fix: tailwind config

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -2,7 +2,7 @@ export function SubmitButton({ children }: { children: React.ReactNode }) {
   return (
     <button
       type="submit"
-      className="border border-black px-8 py-1 bg-sine-green rounded-2xl"
+      className="border border-black px-8 py-1 bg-sine-green rounded-2xl cursor-pointer"
     >
       {children}
     </button>

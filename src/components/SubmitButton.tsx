ef7827497935d4e78c87f90f7cc59b7e6a1d7096
Raimundo Henriques <raimundo.henriques@gmail.com>
2025-03-17 11:46:17 +0100

    feat: add feedback to submit button

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useState } from "react";
 
 interface SubmitButtonProps {
   children: ReactNode;
@@ -6,12 +8,51 @@ interface SubmitButtonProps {
 }
 
 export function SubmitButton({ children, className = "" }: SubmitButtonProps) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   return (
     <button
       type="submit"
-      className={`border border-black px-8 py-1 bg-sine-green rounded-2xl cursor-pointer ${className}`}
+      className={`border border-black px-8 py-1 bg-sine-green rounded-2xl cursor-pointer disabled:cursor-auto ${className} disabled:opacity-35`}
+      onClick={(e) => {
+        e.preventDefault();
+        const form = e.currentTarget.closest("form");
+        if (form) {
+          if (form.checkValidity()) {
+            setIsSubmitting(true);
+            form.requestSubmit();
+          } else {
+            form.reportValidity();
+          }
+        }
+      }}
+      disabled={isSubmitting}
     >
-      {children}
+      {isSubmitting ? (
+        <svg
+          viewBox="0 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="animate-spin w-6 h-6"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M4.5 12.5C4.5 16.9183 8.08172 20.5 12.5 20.5C16.9183 20.5 20.5 16.9183 20.5 12.5C20.5 8.08172 16.9183 4.5 12.5 4.5"
+              stroke="#121923"
+              strokeWidth="1.2"
+            ></path>{" "}
+          </g>
+        </svg>
+      ) : (
+        children
+      )}
     </button>
   );
 }

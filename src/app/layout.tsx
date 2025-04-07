8dfd4ae11acf43a0e042272078ffd71cf352bbab
Raimundo Henriques <raimundo.henriques@gmail.com>
2025-04-07 13:31:19 +0200

    feat: add plausible tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
 import ScrollToTop from "@/components/ScrollToTopWrapper";
+import PlausibleProvider from "next-plausible";
 
 export const metadata: Metadata = {
   title: "Polytune",
@@ -31,29 +32,31 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className={favoritFont.className}>
       <ScrollToTop>
         <body className="bg-sine-green flex justify-center items-center h-screen w-screen p-2">
-          <div className="overflow-y-scroll flex flex-col justify-between items-center text-center bg-white rounded-2xl border border-black h-full w-full">
-            <main className="lg:max-w-6xl flex flex-col items-center justify-center">
-              <div className="my-12 w-xs md:w-lg text-center">
-                <h1 className="-mb-2">Private Multi-Party Benchmarking</h1>
-                <h2>SINE Foundation</h2>
-              </div>
-              {children}
-            </main>
-            <footer className="flex items-end pb-6 pt-12">
-              <a
-                href="https://sine.foundation"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  src="/sine-logo.svg"
-                  alt="SINE logo"
-                  width={100}
-                  height={100}
-                />
-              </a>
-            </footer>
-          </div>
+          <PlausibleProvider domain="benchmarking.sine.dev">
+            <div className="overflow-y-scroll flex flex-col justify-between items-center text-center bg-white rounded-2xl border border-black h-full w-full">
+              <main className="lg:max-w-6xl flex flex-col items-center justify-center">
+                <div className="my-12 w-xs md:w-lg text-center">
+                  <h1 className="-mb-2">Private Multi-Party Benchmarking</h1>
+                  <h2>SINE Foundation</h2>
+                </div>
+                {children}
+              </main>
+              <footer className="flex items-end pb-6 pt-12">
+                <a
+                  href="https://sine.foundation"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src="/sine-logo.svg"
+                    alt="SINE logo"
+                    width={100}
+                    height={100}
+                  />
+                </a>
+              </footer>
+            </div>
+          </PlausibleProvider>
         </body>
       </ScrollToTop>
     </html>
